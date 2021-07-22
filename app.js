0892ff3f91d81fa4a92ce2fd094d7651115101d3
siddusiddartha <siddusiddartha1701@gmail.com>
2021-07-22 20:56:28 +0530

    No slots button

--- app.js
+++ app.js
@@ -76,5 +76,5 @@ res.render("index",{pinCode:pinCode,centerData:centerData});
 
 
 app.listen(process.env.PORT || 3000,function(){
-  console.log("Server is running on port 3000");
+  console.log("Server is running successfully");
 })
